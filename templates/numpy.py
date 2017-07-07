47a01c154296cbe48d92bda2bfc55de3e82baeda
Marton Gunyho <andras.gunyho@aalto.fi>
2017-07-07 09:27:11 +0300

    Add template for numpy files.

---
+++ templates/numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#from mpl_toolkits import mplot3d # 3D plotting
+
+plt.close("all")
+
+
+fig = plt.figure(facecolor = "w")
+ax = fig.gca()
+
+
+
+plt.show()
